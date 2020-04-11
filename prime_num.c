f3bd6f9541abe695264fd0c59dabfabe74a0c3db
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2020-04-11 17:25:15 +0530

    Create prime_num.c

---
+++ prime_num.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int num=100000;
+    for(i=0;i<=num;i++)
+    { for(j=2;j<(num/2);j++)
+        {
+            if((i/2)!=0)
+            {
+                if((i/j)!=0)
+                q[i]=i;
+            }
+            
+        }
+    }
+    printf("the prime numbers upto 1,00,000 are")
+    while(q<100000)
+    {
+        printf("%d",q);
+    }
+    return 0;
+}
