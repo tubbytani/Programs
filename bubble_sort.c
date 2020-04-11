b6afcc7238330861da49413eea294c3c182e06b8
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2020-04-11 17:11:33 +0530

    Update and rename descending order.c to bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,14 +1,18 @@
 
+ 
 #include <stdio.h>
 #include<math.h>
 int main()
 {
-    int q[5],p,temp;
+    int q[10],p,temp;
     int i,j,k;
 	printf("enter 10 numbers \n");
-	for(i=0;i<5;i++)
+	for(i=0;i<10;i++)
 	scanf("%d",&q[i]);
-	for(j=(i+1);j<5;j++)
+	//bubble sort
+	for(i=0;i<10;i++)
+	{
+	for(j=(i+1);j<10;j++)
 	{
 	if(q[j]>q[i])
 	{  
@@ -17,8 +21,9 @@ int main()
 	    q[j]=temp;
 	}
 	}
+	}
 	printf("the sorted numbers in descending order are:");
-	for(i=0;i<5;i++)
+	for(i=0;i<10;i++)
 	{printf("%d",q[i]);}
     return 0;
 }
