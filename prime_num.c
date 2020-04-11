b5b62232fa7489e53fc6f1092772299b0a164714
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2020-04-11 18:00:37 +0530

    Update prime_num.c

--- prime_num.c
+++ prime_num.c
@@ -1,22 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int num=100000;
-    for(i=0;i<=num;i++)
-    { for(j=2;j<(num/2);j++)
+    int num=100,i,j,counter=0;
+     printf("the prime numbers upto 1,00,000 are ");
+  
+    for(i=1;i<=num;i++)
+    { 
+        for(j=1;j<(num/2);j++)
         {
-            if((i/2)!=0)
-            {
-                if((i/j)!=0)
-                q[i]=i;
-            }
+                if((i%j)==0)
+                counter++;
             
         }
+        if(counter==2)
+        printf("%d ",i);
     }
-    printf("the prime numbers upto 1,00,000 are")
-    while(q<100000)
-    {
-        printf("%d",q);
-    }
+   
     return 0;
 }
