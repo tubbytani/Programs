702e8764af28289e2538799ba5769d16f43419da
Tanisha_Bharol <46688469+tubbytani@users.noreply.github.com>
2020-05-19 15:59:59 +0530

    Update networking.py

--- python/networking.py
+++ python/networking.py
@@ -4,4 +4,4 @@ r=req.get(url)
 content=r.text
 lowerlim=content.index('<p>')
 upperlim=content.index('</p>')
-print(content[lowerlim+4:upperlim])
+print(content[lowerlim+4:upperlim]) #shifts 4 letters in the content from lowerlimit 
